456ff0e1792f69d13d55996cc86d8620d755ae14
flower1 <email@sample.com>
2023-02-16 16:43:51 +0800

    update scrollview componente and style

--- src/base-ui/scroll-view/index.jsx
+++ src/base-ui/scroll-view/index.jsx
@@ -2,13 +2,16 @@ import PropTypes from "prop-types"
 import React, { memo } from "react"
 import { useEffect, useState, useRef } from "react"
 import { ViewWrapper } from "./style"
+import IconArrowLeft from "@/assets/svg/icon-arrow-left"
+import IconArrowRight from "@/assets/svg/icon-arrow-right"
 
 const ScrollView = memo(props => {
 	// 定义内部状态
 	const [showRight, setRight] = useState(false)
+	const [showLeft, setLeft] = useState(false)
 	const [posIndex, setPosIndex] = useState(0)
 
-  const totalDistanceRef = useRef()
+	const totalDistanceRef = useRef()
 	// 组件渲染完毕判断是否显示右侧按钮
 	const scrollContentRef = useRef()
 	useEffect(() => {
@@ -16,34 +19,36 @@ const ScrollView = memo(props => {
 		const clientWidth = scrollContentRef.current?.clientWidth
 		const totalDistance = scrollWidth - clientWidth
 		setRight(totalDistance > 0)
-    totalDistanceRef.current = totalDistance
+		totalDistanceRef.current = totalDistance
 	}, [props.children])
 
 	// 事件处理逻辑
-	function rightClickHandle() {
-		const newIndex = posIndex + 1
+	function controlClickHandle(direction) {
+		const newIndex = posIndex + direction
 		const newEl = scrollContentRef.current.children[newIndex]
-    const newOffsetLeft = newEl.offsetLeft
-
+		const newOffsetLeft = newEl.offsetLeft
 		scrollContentRef.current.style.transform = `translate(-${newOffsetLeft}px)`
 		setPosIndex(newIndex)
-    setRight(totalDistanceRef.current > newOffsetLeft)
+		setRight(totalDistanceRef.current > newOffsetLeft)
+		setLeft(newOffsetLeft > 0)
 	}
-	function leftClickHandle() {
-		const newIndex = posIndex - 1
-		const newEl = scrollContentRef.current.children[newIndex]
-		if (newEl) {
-			scrollContentRef.current.style.transform = `translate(-${newEl.offsetLeft}px)`
-			setPosIndex(newIndex)
-		}
-	}
+
 	return (
 		<ViewWrapper>
-			<button onClick={leftClickHandle}>左边按钮</button>
-			{showRight && <button onClick={rightClickHandle}>右边按钮</button>}
-
-			<div className="scroll-content" ref={scrollContentRef}>
-				{props.children}
+			{showLeft && (
+				<div className="control left" onClick={e => controlClickHandle(-1)}>
+					<IconArrowLeft></IconArrowLeft>
+				</div>
+			)}
+			{showRight && (
+				<div className="control right" onClick={e => controlClickHandle(1)}>
+					<IconArrowRight></IconArrowRight>
+				</div>
+			)}
+			<div className="scroll">
+				<div className="scroll-content" ref={scrollContentRef}>
+					{props.children}
+				</div>
 			</div>
 		</ViewWrapper>
 	)
--- src/base-ui/scroll-view/style.js
+++ src/base-ui/scroll-view/style.js
@@ -1,11 +1,40 @@
 import styled from "styled-components"
 
 export const ViewWrapper = styled.div`
-	overflow: hidden;
-	.scroll-content {
-		display: flex;
-		position: relative;
+	position: relative;
+	padding: 8px 0;
+	.scroll {
+		overflow: hidden;
 
-		transition: transform 250ms ease;
+		.scroll-content {
+			display: flex;
+
+			transition: transform 250ms ease;
+		}
+	}
+	.control {
+		position: absolute;
+		z-index: 9;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 28px;
+		height: 28px;
+		border-radius: 50%;
+		text-align: center;
+		border: 2px solid #fff;
+		background-color: #fff;
+		box-shadow: 0px 1px 1px 1px rgba(0, 0, 0, 0.14);
+		cursor: pointer;
+		&.left {
+			left: 0;
+			top: 50%;
+			transform: translate(-50%, -50%);
+		}
+		&.right {
+			right: 0;
+			top: 50%;
+			transform: translate(50%, -50%);
+		}
 	}
 `
