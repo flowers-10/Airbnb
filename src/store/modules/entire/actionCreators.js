afd67d647aee68b036a09bc2ca2e1033b5ba6728
flowers-10 <mgaligigi@163.com>
2023-03-26 10:53:05 +0800

    feat:点击分页请求数据

--- src/store/modules/entire/actionCreators.js
+++ src/store/modules/entire/actionCreators.js
@@ -1,38 +1,37 @@
-import { getEntireRoomList } from "@/services/modules/entire";
-import * as actionTypes from "./constants";
+import { getEntireRoomList } from "@/services/modules/entire"
+import * as actionTypes from "./constants"
 
-export const changeLoadingAction = (isLoading) => ({
-  type: actionTypes.CHANGE_LOADING,
-  isLoading
+export const changeLoadingAction = isLoading => ({
+	type: actionTypes.CHANGE_LOADING,
+	isLoading,
 })
 
-export const changeCurrentPageAction = (currentPage) => ({
-  type: actionTypes.CHANGE_CURRENT_PAGE,
-  currentPage
+export const changeCurrentPageAction = currentPage => ({
+	type: actionTypes.CHANGE_CURRENT_PAGE,
+	currentPage,
 })
 
-export const changeTotalCountAction = (totalCount) => ({
-  type: actionTypes.CHANGE_TOTAL_COUNT,
-  totalCount
+export const changeTotalCountAction = totalCount => ({
+	type: actionTypes.CHANGE_TOTAL_COUNT,
+	totalCount,
 })
 
-export const changeRoomListAction = (roomList) => ({
-  type: actionTypes.CHANGE_ROOM_LIST,
-  roomList
+export const changeRoomListAction = roomList => ({
+	type: actionTypes.CHANGE_ROOM_LIST,
+	roomList,
 })
-
 
 export const fetchEntireDataAction = (page = 0) => {
-  return async dispatch => {
-    // 设置isLoading
-    dispatch(changeLoadingAction(true))
+	return async dispatch => {
+		dispatch(changeCurrentPageAction(page))
 
-    const res = await getEntireRoomList(page * 20)
-    // console.log(res);
-    dispatch(changeLoadingAction(false))
-    // 保存数据
-    dispatch(changeCurrentPageAction(page))
-    dispatch(changeTotalCountAction(res.data.totalCount))
-    dispatch(changeRoomListAction(res.data.list))
-  }
+		// 设置isLoading
+		dispatch(changeLoadingAction(true))
+		const res = await getEntireRoomList(page * 20)
+		// console.log(res);
+		dispatch(changeLoadingAction(false))
+		// 保存数据
+		dispatch(changeTotalCountAction(res.data.totalCount))
+		dispatch(changeRoomListAction(res.data.list))
+	}
 }
--- src/views/entire/c-cpns/entire-pagination/index.jsx
+++ src/views/entire/c-cpns/entire-pagination/index.jsx
@@ -1,7 +1,8 @@
 import React, { memo } from "react"
 import { PaginationWrapper } from "./style"
 import { Pagination } from "antd"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchEntireDataAction } from "@/store/modules/entire/actionCreators"
 
 const EntirePagination = memo(props => {
 	const { totalCount, currentPage, roomList } = useSelector(state => ({
@@ -9,14 +10,22 @@ const EntirePagination = memo(props => {
 		currentPage: state.entire.currentPage,
 		roomList: state.entire.roomList,
 	}))
-  const totalPage = ~~(totalCount /20)
 	const startCount = currentPage * 20 + 1
 	const endCount = (currentPage + 1) * 20
+
+  // 事件处理逻辑
+  const dispatch = useDispatch()
+  function pageChangeHandle(page,pageSize) {
+    // 更新最新页码 redux => currentPages
+    console.log(page);
+    // dispatch(changeCurrentPageAction(page-1))
+    dispatch(fetchEntireDataAction(page-1))
+  }
 	return (
 		<PaginationWrapper>
 			{ !!roomList.length && (
 				<div className="info">
-					<Pagination className="dePag" defaultCurrent={1} total={300} defaultPageSize={20}/>
+					<Pagination className="dePag" defaultCurrent={1} total={300} defaultPageSize={20} pageSize={20} onChange={ pageChangeHandle}/>
 					<div className="desc">
 						第 {startCount} - {endCount} 个房源，共超过 {totalCount} 个
 					</div>
