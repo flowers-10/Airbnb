3312afdf87f424011097eaefa4824564454ab570
flowers-10 <mgaligigi@163.com>
2023-03-26 10:37:33 +0800

    feat(entire-pagination): 更新分页器

--- src/views/entire/c-cpns/entire-pagination/index.jsx
+++ src/views/entire/c-cpns/entire-pagination/index.jsx
@@ -1,13 +1,29 @@
-import PropTypes from 'prop-types'
-import React, { memo } from 'react'
-import { PaginationWrapper } from './style'
+import React, { memo } from "react"
+import { PaginationWrapper } from "./style"
+import { Pagination } from "antd"
+import { useSelector } from "react-redux"
 
-const EntirePagination = memo((props) => {
-  return (
-    <PaginationWrapper>EntireFilter</PaginationWrapper>
-  )
+const EntirePagination = memo(props => {
+	const { totalCount, currentPage, roomList } = useSelector(state => ({
+		totalCount: state.entire.totalCount,
+		currentPage: state.entire.currentPage,
+		roomList: state.entire.roomList,
+	}))
+  const totalPage = ~~(totalCount /20)
+	const startCount = currentPage * 20 + 1
+	const endCount = (currentPage + 1) * 20
+	return (
+		<PaginationWrapper>
+			{ !!roomList.length && (
+				<div className="info">
+					<Pagination className="dePag" defaultCurrent={1} total={300} defaultPageSize={20}/>
+					<div className="desc">
+						第 {startCount} - {endCount} 个房源，共超过 {totalCount} 个
+					</div>
+				</div>
+			)}
+		</PaginationWrapper>
+	)
 })
-
-EntirePagination.propTypes = {}
 
 export default EntirePagination
--- src/views/entire/c-cpns/entire-pagination/style.js
+++ src/views/entire/c-cpns/entire-pagination/style.js
@@ -1,7 +1,28 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const PaginationWrapper = styled.div`
-
-
-
+		display: flex;
+		justify-content: center;
+		.info {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+      .desc {
+        margin-top: 16px;
+      }
+		}
+    
+	.dePag {
+		.ant-pagination-options {
+			display: none !important;
+		}
+		.ant-pagination-item-active {
+			background-color: #222;
+			border: none;
+			border-radius: 50%;
+			a {
+				color: #fff;
+			}
+		}
+	}
 `
