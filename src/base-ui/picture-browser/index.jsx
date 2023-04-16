574430e4c53ba78d47cdf1ac45c77b238d6dbe25
flowers-10 <mgaligigi@163.com>
2023-04-16 09:13:34 +0800

    feat:实现图片轮播功能

--- src/base-ui/picture-browser/index.jsx
+++ src/base-ui/picture-browser/index.jsx
@@ -1,11 +1,16 @@
+import IconArrowLeft from "@/assets/svg/icon-arrow-left"
+import IconArrowRight from "@/assets/svg/icon-arrow-right"
 import IconClose from "@/assets/svg/icon-close"
 import PropTypes from "prop-types"
 import React, { memo } from "react"
+import { useState } from "react"
 import { useEffect } from "react"
 import { BrowserWrapper } from "./style"
 
 const PictureBrowser = memo(props => {
 	const { pictureUrls, closeClick } = props
+
+	const [currentIndex, setCurrentIndex] = useState(0)
 
 	useEffect(() => {
 		document.body.style = "overflow:hidden"
@@ -17,15 +22,34 @@ const PictureBrowser = memo(props => {
 		}
 	}
 
+	function controlClickHandle(isNext = true) {
+		let newIndex = isNext ? currentIndex + 1 : currentIndex - 1
+		if (newIndex < 0) newIndex = pictureUrls.length - 1
+		if (newIndex > pictureUrls.length - 1) newIndex = 0
+		setCurrentIndex(newIndex)
+	}
+
 	return (
 		<BrowserWrapper>
 			<div className="top">
-				<div className="close-btn" onClick={e =>closeBtnClickHandle()} >
+				<div className="close-btn" onClick={e => closeBtnClickHandle()}>
 					<IconClose />
 				</div>
 			</div>
-			<div className="list"></div>
-			<div className="indicator"></div>
+			<div className="slider">
+				<div className="control">
+					<div className="btn left" onClick={e => controlClickHandle(false)} >
+						<IconArrowLeft width="77" height="77" />
+					</div>
+					<div className="picture">
+						<img src={pictureUrls[currentIndex]} alt="" />
+					</div>
+					<div className="btn right" onClick={e => controlClickHandle(true)}>
+						<IconArrowRight width="77" height="77"  />
+					</div>
+				</div>
+			</div>
+			<div className="preview"></div>
 		</BrowserWrapper>
 	)
 })
