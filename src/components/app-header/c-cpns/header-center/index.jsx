57df0d0e2fc1e328fc03707ce508d1e2106df5c4
flowers-10 <mgaligigi@163.com>
2023-04-22 21:13:39 +0800

    feat(AppHeader):搜索切换的动画效果

--- src/components/app-header/c-cpns/header-center/index.jsx
+++ src/components/app-header/c-cpns/header-center/index.jsx
@@ -1,5 +1,5 @@
 import React, { memo } from "react"
-
+import { CSSTransition } from "react-transition-group"
 import SearchTitles from "@/assets/data/search_titles.json"
 import { CenterWrapper } from "./style"
 import IconSearchBar from "@/assets/svg/icon-search-bar"
@@ -8,23 +8,35 @@ import SearchSections from "./c-cpns/search-sections"
 import { useState } from "react"
 
 const HeaderCenter = memo(props => {
+	const { isSearch, searchBarClick } = props
+
 	const [tabIndex, setTabIndex] = useState(0)
 	const titles = SearchTitles.map(item => item.title)
 
+	function searchBarClickHandle() {
+		if (searchBarClick) {
+			searchBarClick()
+		}
+	}
+
 	return (
 		<CenterWrapper>
-			{/* <div className="search-bar">
-				<div className="text">搜索房源和体验</div>
-				<div className="icon">
-					<IconSearchBar />
+			<CSSTransition in={!isSearch} classNames="bar" timeout={250} unmountOnExit={true}>
+				<div className="search-bar" onClick={searchBarClickHandle}>
+					<div className="text">搜索房源和体验</div>
+					<div className="icon">
+						<IconSearchBar />
+					</div>
 				</div>
-			</div> */}
+			</CSSTransition>
+			<CSSTransition in={isSearch} classNames="bar" timeout={250} unmountOnExit={true}>
 			<div className="search-detail">
 				<SearchTabs titles={[...titles]} tabClick={setTabIndex} />
 				<div className="infos">
 					<SearchSections searchInfos={SearchTitles[tabIndex].searchInfos} />
 				</div>
 			</div>
+			</CSSTransition>
 		</CenterWrapper>
 	)
 })
--- src/components/app-header/index.jsx
+++ src/components/app-header/index.jsx
@@ -1,4 +1,4 @@
-import React, { memo } from "react"
+import React, { memo, useState } from "react"
 import { shallowEqual, useSelector } from "react-redux"
 import HeaderCenter from "./c-cpns/header-center"
 import HeaderLeft from "./c-cpns/header-left"
@@ -7,6 +7,8 @@ import { HeaderWrapper, SearchAreaPlaceholder } from "./style"
 import classNames from "classnames"
 
 const AppHeader = memo(props => {
+	//  定义组件内部状态
+	const [isSearch, setIsSearch] = useState(false)
 	// 从rudex中获取数据
 	const { headerConfig } = useSelector(
 		state => ({
@@ -21,12 +23,12 @@ const AppHeader = memo(props => {
 			<div className="content">
 				<div className="top">
 					<HeaderLeft />
-					<HeaderCenter />
+					<HeaderCenter  isSearch={isSearch} searchBarClick={e => setIsSearch(true)}/>
 					<HeaderRight />
 				</div>
-				<SearchAreaPlaceholder />
+				<SearchAreaPlaceholder isSearch={isSearch}/>
 			</div>
-      <div className="cover"></div>
+			{isSearch && <div className="cover" onClick={e => setIsSearch(false)}></div>}
 		</HeaderWrapper>
 	)
 })
